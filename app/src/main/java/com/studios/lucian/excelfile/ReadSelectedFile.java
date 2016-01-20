29adfc6e0c549f96385a0179e813acc7a280049c
Lucian Iacob <luciano.iacob@gmail.com>
2016-01-20 15:44:11 +0200

    In the Activity ReadSelectedFile we have an arrow to go to previous activity and we also have a list of items

--- app/src/main/java/com/studios/lucian/excelfile/ReadSelectedFile.java
+++ app/src/main/java/com/studios/lucian/excelfile/ReadSelectedFile.java
@@ -1,12 +1,20 @@
 package com.studios.lucian.excelfile;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class ReadSelectedFile extends AppCompatActivity {
+
+    private ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +23,32 @@ public class ReadSelectedFile extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
         Intent intent = getIntent();
         String file_path = intent.getStringExtra(getString(R.string.file_absolute_path));
-        new AlertDialog.Builder(this).setTitle(file_path).setPositiveButton("OK", null).show();
+        parseExcelFile(file_path);
+        displayStudents();
     }
 
+    private void displayStudents() {
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(this, R.layout.row, list);
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        listView.setAdapter(stringArrayAdapter);
+    }
+
+    private void parseExcelFile(String file_path) {
+        try {
+            File file = new File(file_path);
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            list = new ArrayList<>();
+            list.add("Toni");
+            list.add("Alex");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
