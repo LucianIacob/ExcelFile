439d06c7604cf6c1cd1a804b10c63fe6379dd36e
Lucian Iacob <luciano.iacob@gmail.com>
2016-01-20 16:37:58 +0200

    First work-able parsing file

--- app/src/main/java/com/studios/lucian/excelfile/ReadSelectedFile.java
+++ app/src/main/java/com/studios/lucian/excelfile/ReadSelectedFile.java
@@ -1,5 +1,6 @@
 package com.studios.lucian.excelfile;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +10,13 @@ import android.widget.ListView;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 
 public class ReadSelectedFile extends AppCompatActivity {
 
@@ -25,7 +31,7 @@ public class ReadSelectedFile extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
+
         Intent intent = getIntent();
         String file_path = intent.getStringExtra(getString(R.string.file_absolute_path));
         parseExcelFile(file_path);
@@ -40,15 +46,24 @@ public class ReadSelectedFile extends AppCompatActivity {
 
     private void parseExcelFile(String file_path) {
         try {
+            list = new ArrayList<>();
             File file = new File(file_path);
             FileInputStream fileInputStream = new FileInputStream(file);
 
-            list = new ArrayList<>();
-            list.add("Toni");
-            list.add("Alex");
+            Workbook workbook = Workbook.getWorkbook(fileInputStream);
+            Sheet sheet = workbook.getSheet(0);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            int row = sheet.getRows();
+            String listItem;
+            for (int i = 8; i < 127; i++) {
+                listItem = "";
+                Cell cell = sheet.getCell(1, i);
+                listItem += cell.getContents();
+
+                list.add(listItem);
+            }
+        } catch (BiffException | IOException e) {
+            new AlertDialog.Builder(this).setTitle(e.getMessage()).setPositiveButton("OK", null).show();
         }
     }
 }
